b62376496848f48745405d15e33732390c17b7b8
Mathieu Santostefano <msantostefano@protonmail.com>
2024-10-24 14:06:29 +0200

    Add webhooks signature verification on Sweego bridges

--- Tests/Webhook/SweegoWrongSignatureRequestParserTest.php
+++ Tests/Webhook/SweegoWrongSignatureRequestParserTest.php
@@ -15,12 +15,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\Bridge\Sweego\RemoteEvent\SweegoPayloadConverter;
 use Symfony\Component\Mailer\Bridge\Sweego\Webhook\SweegoRequestParser;
 use Symfony\Component\Webhook\Client\RequestParserInterface;
+use Symfony\Component\Webhook\Exception\RejectWebhookException;
 use Symfony\Component\Webhook\Test\AbstractRequestParserTestCase;
 
-class SweegoRequestParserTest extends AbstractRequestParserTestCase
+class SweegoWrongSignatureRequestParserTest extends AbstractRequestParserTestCase
 {
     protected function createRequestParser(): RequestParserInterface
     {
+        $this->expectException(RejectWebhookException::class);
+        $this->expectExceptionMessage('Invalid signature.');
+
         return new SweegoRequestParser(new SweegoPayloadConverter());
     }
 
@@ -30,7 +34,7 @@ class SweegoRequestParserTest extends AbstractRequestParserTestCase
             'Content-Type' => 'application/json',
             'HTTP_webhook-id' => '9f26b9d0-13d7-410c-ba04-5019cd30e6d0',
             'HTTP_webhook-timestamp' => '1723737959',
-            'HTTP_webhook-signature' => 'W+fm4VPshCGjuT0HxyV00QEbFitZd2Rdvx82bWM7VXc=',
+            'HTTP_webhook-signature' => 'wrong_signature',
         ], $payload);
     }
 }
