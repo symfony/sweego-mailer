ff26f43da9b291a222cd425ac1190e6eead17f70
Mathieu Santostefano <msantostefano@proton.me>
2025-08-29 11:28:42 +0200

    Add missing Sweego Mailer Bridge webhook events

--- Tests/Webhook/SweegoSignedRequestParserTest.php
+++ Tests/Webhook/SweegoSignedRequestParserTest.php
@@ -17,20 +17,36 @@ use Symfony\Component\Mailer\Bridge\Sweego\Webhook\SweegoRequestParser;
 use Symfony\Component\Webhook\Client\RequestParserInterface;
 use Symfony\Component\Webhook\Test\AbstractRequestParserTestCase;
 
-class SweegoRequestParserTest extends AbstractRequestParserTestCase
+class SweegoSignedRequestParserTest extends AbstractRequestParserTestCase
 {
     protected function createRequestParser(): RequestParserInterface
     {
         return new SweegoRequestParser(new SweegoPayloadConverter());
     }
 
+    public static function getPayloads(): iterable
+    {
+        $filename = 'delivered.json';
+        $currentDir = \dirname((new \ReflectionClass(static::class))->getFileName());
+
+        yield $filename => [
+            file_get_contents($currentDir.'/Fixtures/delivered.json'),
+            include ($currentDir.'/Fixtures/delivered.php'),
+        ];
+    }
+
+    protected function getSecret(): string
+    {
+        return 'GvLY88Uyj70jQm3fUwYyWmAaiz98wWim';
+    }
+
     protected function createRequest(string $payload): Request
     {
         return Request::create('/', 'POST', [], [], [], [
             'Content-Type' => 'application/json',
-            'HTTP_webhook-id' => 'id',
-            'HTTP_webhook-timestamp' => 'timestamp',
-            'HTTP_webhook-signature' => 'signature',
+            'HTTP_webhook-id' => '9f26b9d0-13d7-410c-ba04-5019cd30e6d0',
+            'HTTP_webhook-timestamp' => '1723737959',
+            'HTTP_webhook-signature' => 'E1RfmN81xnbXMqDZUD0eJjPQEWmf24ft//gtV29bp18=',
         ], $payload);
     }
 }
